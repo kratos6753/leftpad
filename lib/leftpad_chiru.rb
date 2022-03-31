5bd93c7e528d6487ce5c9b98b3de84e2c6cbb4d1
Chiranjeevi Kodati <chiru6753@gmail.com>
2022-03-31 21:40:46 +0530

    merged

---
+++ lib/leftpad_chiru.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative "leftpad_chiru/version"
+
+module LeftpadChiru
+  class Error < StandardError; end
+  
+  def leftpad(chars, filler=' ')
+    self.rjust(chars, filler)
+  end
+
+end
+
+class String
+  include LeftpadChiru
+end
---
+++ spec/leftpad_chiru_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+RSpec.describe LeftpadChiru do
+  
+  it "has a version number" do
+    expect(LeftpadChiru::VERSION).not_to be_nil
+  end
+
+  it "leftpad with no padding char" do
+    expect('left'.leftpad(8)).to eq('    left')
+  end
+
+  it "leftpad with padding char" do
+    expect('left'.leftpad(8, '0')).to eq('0000left')
+  end
+
+  it "leftpad with fewer padding char" do
+    expect('left'.leftpad(2, '0')).to eq('left')
+  end
+
+end
